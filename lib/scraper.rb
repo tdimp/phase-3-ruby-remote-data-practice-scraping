3a9d2c3cfcdfd7a5e665caf5c3791d405836ab34
Thomas Dimperio <twdimperio@gmail.com>
2022-04-27 21:07:06 -0600

    commit tree d382141ee0f13017b6679a54ff1011ee723343f1

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+PAGE_URL = URI.open("http://ruby.bastardsbook.com/files/hello-webpage.html")
+page = Nokogiri::HTML(PAGE_URL)
+p page.css("div#funstuff").text
